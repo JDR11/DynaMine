772626446b75a0880dc57c2b80a4a2682b378c2b
JDR11 <43808975+JDR11@users.noreply.github.com>
2021-04-30 15:40:36 +0100

    Update bot.js
    Reworked bot creation to use values from .env file instead of visible in code. Increases security.

--- bot.js
+++ bot.js
@@ -1,3 +1,5 @@
+const express = require("express");
+require("dotenv").config();
 const mineflayer = require("mineflayer");
 const mineEntity = require("prismarine-entity");
 const Vec3 = require("vec3").Vec3;
@@ -7,8 +9,8 @@ function createBot () {
   const bot = mineflayer.createBot({
   //  host: "192.168.8.106",
     host: "minecraft.cabotexplorers.org.uk",
-    username: "joshua.ruddock27@outlook.com",
-    password: "vNxD74TLVEuGmLH",
+    username: process.env.ACCOUNT_EMAIL,
+    password: process.env.ACCOUNT_PASSWORD,
     version: false
   })
 
