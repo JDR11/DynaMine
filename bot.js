9a394e90d0cb692da543510ad4e89c55831388d9
JDR11 <joshua.ruddock27@outlook.com>
2022-01-22 01:28:05 +0000

    Moved to linux system

--- bot.js
+++ bot.js
@@ -1,26 +1,25 @@
 const express = require("express");
 require("dotenv").config();
 const mineflayer = require("mineflayer");
-const mineEntity = require("prismarine-entity");
-const Vec3 = require("vec3").Vec3;
+//const mineEntity = require("prismarine-entity");
+//const Vec3 = require("vec3").Vec3;
 const sleep = require("sleep");
 
 function createBot () {
-  const bot = mineflayer.createBot({
-  //  host: "192.168.8.106",
-    host: "minecraft.cabotexplorers.org.uk",
-    username: process.env.ACCOUNT_EMAIL,
-    password: process.env.ACCOUNT_PASSWORD,
-    version: false
-  })
+    const bot = mineflayer.createBot({
+        host: "192.168.8.106",
+        username: process.env.ACCOUNT_EMAIL,
+        password: process.env.ACCOUNT_PASSWORD,
+        version: false
+    })
 
-  bot.on("error", err => console.log(err));
-  bot.on("end", createBot);
-  bot.on("spawn", () => {
-    console.log("spawned");
-    sleep.sleep(600);
-    console.log(bot.entity);
-  })
+    bot.on("error", err => console.log(err));
+    bot.on("end", createBot);
+    bot.on("spawn", () => {
+        console.log("spawned");
+        sleep.sleep(600);
+        console.log(bot.entity);
+    })
 }
 
 createBot()
